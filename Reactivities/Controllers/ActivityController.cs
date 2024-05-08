d5448b4207d4bd017fe85a9ce240b937cbf2f255
Saad <stcodeslab@gmail.com>
2024-05-09 03:20:10 +0500

    End of Section 2

---
+++ Reactivities/Controllers/ActivityController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain;
+using Reactivities.Persistence;
+
+namespace Reactivities.Controllers
+{
+    public class ActivityController :BaseAPIController
+    {
+        private readonly DataContext _context;
+
+        public ActivityController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivity()
+        {
+            return await _context.Activities.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Activity>>GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
+    }
+}
---
+++ Reactivities/Controllers/BaseAPIController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.Domain;
+
+namespace Reactivities.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseAPIController : ControllerBase
+    {
+
+       
+    }
+}
